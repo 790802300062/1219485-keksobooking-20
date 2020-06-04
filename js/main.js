fbf94e5a7d8acb0f1f7a4396247900334b3b2de5
vyacheslav <vyachesl.n@gmail.com>
2020-06-04 14:35:28 +0600

    Исправляет ошибки

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ var TYPES = ['palace', 'flat', 'house', 'bungalo'];
 var CHECKINS = ['12:00', '13:00', '14:00'];
 var CHECKOUTS = ['12:00', '13:00', '14:00'];
 var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg']
+var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 
 // Наполняем массив для аватарок
 for (var i = 1; i <= ADS_AMOUNT; i++) {
@@ -20,44 +20,45 @@ for (var i = 1; i <= ADS_AMOUNT; i++) {
 }
 
 // Функция "перемешивания" массива
-function shuffleArray (array) {
-  var j, temp;
-  for (var i = array.length - 1; i > 0; i--) {
+function shuffleArray(array) {
+  var j;
+  var temp;
+  for (i = array.length - 1; i > 0; i--) {
     j = Math.floor(Math.random() * (i + 1));
     temp = array[j];
     array[j] = array [i];
     array [i] = temp;
   }
   return array;
-};
+}
 
-//Функция получения случайного значения из диапазона
-function getRandomNumber (start, end) {
+// Функция получения случайного значения из диапазона
+function getRandomNumber(start, end) {
   var randomNumber = Math.floor(Math.random() * (end - start) + start);
   return randomNumber;
-};
+}
 
 // Функция получения случайного номера элемента массива
-function getRandomElement (array) {
+function getRandomElement(array) {
   var randomElement = array[Math.floor(Math.random() * array.length)];
   return randomElement;
-};
+}
 
 // Функция получения неповторяющегося массива случайной длины
-function getRandomArray (array) {
+function getRandomArray(array) {
   var randomNumberOfElements = Math.ceil(Math.random() * array.length);
   var newArray = array;
   var randomArray = [];
 
-  for (var i = 0; i < randomNumberOfElements; i++) {
+  for (i = 0; i < randomNumberOfElements; i++) {
     randomNumberOfElements = Math.floor(Math.random() * newArray.length);
     randomArray[i] = newArray[randomNumberOfElements];
     newArray.splice(randomNumberOfElements, 1);
-  };
+  }
   return randomArray;
-};
+}
 
-shuffleArray(AVATARS)
+shuffleArray(AVATARS);
 
 function createAdv() {
   var locationX = getRandomNumber(MIN_X, MAX_X);
@@ -81,19 +82,19 @@ function createAdv() {
       'photos': getRandomArray(PHOTOS)
     },
 
-    'location' : {
+    'location': {
       'x': locationX,
       'y': locationY
     }
   };
   return adv;
-};
+}
 
 var advertisements = [];
-for (var i = 0; i < ADS_AMOUNT; i++) {
+for (i = 0; i < ADS_AMOUNT; i++) {
   var ad = createAdv();
   advertisements.push(ad);
-};
+}
 
 document.querySelector('.map').classList.remove('map--faded');
 
@@ -101,8 +102,8 @@ var pin = document.querySelector('#pin')
           .content
           .querySelector('.map__pin');
 
-//Функция создания метки
-function createPin (advArray) {
+// Функция создания метки
+function createPin(advArray) {
   var newPin = pin.cloneNode(true);
   newPin.querySelector('img').src = advArray.author.avatar;
   newPin.querySelector('img').alt = advArray.offer.title;
@@ -112,9 +113,9 @@ function createPin (advArray) {
 }
 
 var pinsCollection = document.createDocumentFragment();
-for (var i = 0; i < advertisements.length; i++) {
+for (i = 0; i < advertisements.length; i++) {
   pinsCollection.appendChild(createPin(advertisements[i]));
-};
+}
 
 document.querySelector('.map__pins').appendChild(pinsCollection);
 
